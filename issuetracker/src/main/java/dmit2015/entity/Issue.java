397e4f6f184411d92c05043f56c1e32a1465a14b
Sam Wu <swu@nait.ca>
2019-04-15 20:19:09 -0600

    Added part 3

--- issuetracker/src/main/java/dmit2015/entity/Issue.java
+++ issuetracker/src/main/java/dmit2015/entity/Issue.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +13,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -28,6 +31,8 @@ public class Issue implements Serializable {
 	@Column(nullable=false, length=99)
 	private String issueLabel;
 	
+//	@JsonbDateFormat(value="yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
 	@NotNull(message="Enter or select the Date of the issue")
 	@Column(nullable=false)
 	@Temporal(TemporalType.TIMESTAMP)
---
+++ issuetracker/src/main/java/dmit2015/resource/IssueResource.java
@@ -0,0 +1,74 @@
+package dmit2015.resource;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import dmit2015.entity.Issue;
+import dmit2015.service.IssueService;
+
+@Path("issues")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class IssueResource {
+	
+	@Inject
+	private IssueService issueService;
+	
+	@GET
+	public List<Issue> findAll() {
+		return issueService.findAllIssue();
+	}
+	
+	@GET
+	@Path("{id}")
+	public Issue findOneById(@PathParam("id") long issueId) {
+		return issueService.findOneIssue(issueId);
+	}
+	
+	@POST
+	public void create(Issue newIssue) {
+		issueService.createIssue(newIssue);
+	}
+
+	@PUT
+	@Path("{id}")
+	public void update(@PathParam("id") long issueId, Issue existingIssue) {
+		existingIssue.setIssueId(issueId);
+		issueService.updateIssue(existingIssue);
+	}
+	
+	@DELETE
+	@Path("{id}")
+	public void delete(@PathParam("id") int issueId) {
+		Issue existingIssue = issueService.findOneIssue(issueId);
+		if (existingIssue != null) {
+			issueService.deleteIssue(existingIssue);			
+		}
+	}
+
+	@GET
+	@Path("hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloPlainText() {
+		return "JAX-RS Plain Text Message";
+	}
+	
+	@GET
+	@Path("hello")
+	@Produces(MediaType.TEXT_HTML)
+	public String helloHtmlText() {
+		return "<p>JAX-RS <strong>HTML</strong> Text Message</p>";
+	}
+
+	
+}
